63b6ce96d3f1a1a7fd20ac270286d1e1db963c9f
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2023-03-09 21:45:31 +0900

    complete 1-module\1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0) {
+    return 1;
+  }
+
+  let theProduct = 1;
+
+  for (let i = n; i > 0; i--) {
+    theProduct *= i;
+  }
+
+  return theProduct;
 }
