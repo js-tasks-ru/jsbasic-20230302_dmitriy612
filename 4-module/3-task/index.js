2bcd6e75a6e6863a31e78852f523043cc4f62f92
Dmitriy Yugay <dmitriy2002yoo@gmail.com>
2023-03-29 16:58:52 +0900

    complete 4-module\3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,31 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    const tr = table.rows[i];
+
+    switch (tr.cells[2].innerHTML) {
+    case "m":
+      tr.classList.add("male");
+      break;
+    case "f":
+      tr.classList.add("female");
+      break;
+    }
+
+    if (Number(tr.cells[1].innerHTML) < 18) {
+      tr.style.textDecoration = "line-through";
+    }
+
+    const lastTd = tr.lastElementChild;
+
+    if (lastTd.hasAttribute("data-available")) {
+      if (lastTd.getAttribute("data-available") === "true") {
+        tr.classList.add("available");
+        continue;
+      }
+
+      tr.classList.add("unavailable");
+    } else {
+      tr.setAttribute("hidden", "");
+    }
+  }
 }
